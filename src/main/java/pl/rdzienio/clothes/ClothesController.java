c73ce1a62fd1c6ef4cd654c60a825f150624548a
Robert Dzienio <robertdzienio@gmail.com>
2020-06-20 20:10:29 +0200

    [fix] weather description

--- src/main/java/pl/rdzienio/clothes/ClothesController.java
+++ src/main/java/pl/rdzienio/clothes/ClothesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clothes")
+@RequestMapping("/api/clothes")
 public class ClothesController {
 
     private final ClothesService service;
