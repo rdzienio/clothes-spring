9dc7cd2860f650cbdfaf1bf3d13d49c93dadc699
Robert Dzienio <robertdzienio@gmail.com>
2021-04-18 12:24:52 +0200

    [add] clothesList.html page

--- src/main/java/pl/rdzienio/clothes/ClothesController.java
+++ src/main/java/pl/rdzienio/clothes/ClothesController.java
@@ -2,48 +2,39 @@ package pl.rdzienio.clothes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.rdzienio.weather.WeatherService;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/clothes")
+@Controller
+@RequestMapping("")
 public class ClothesController {
 
-    private final ClothesService service;
-    private final ClothesConverter converter;
+    private final ClothesService clothesService;
+    private final WeatherService weatherService;
     Logger logger = LoggerFactory.getLogger(ClothesController.class);
 
-    public ClothesController(final ClothesService service, ClothesConverter converter) {
-        this.service = service;
-        this.converter = converter;
+
+    public ClothesController(ClothesService clothesService, WeatherService weatherService) {
+        this.clothesService = clothesService;
+        this.weatherService = weatherService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Clothes>> getAllClothes() {
-        return ResponseEntity.ok(service.getAll());
+    public String showHomePageWithWeather(Model model)
+    {
+        model.addAttribute("weatherForWarsaw", weatherService.getWeatherForecast("Warszawa"));
+        return "index";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ClothesDTO> getById(@PathVariable Integer id) {
-        logger.info("Got id {}", id);
-        Clothes gotById = service.getById(id);
-        logger.info("Found with id {}: {}", id, gotById.getName());
-        return ResponseEntity.ok(converter.fromClothes(gotById));
+    @GetMapping("clothes")
+    public String showClothesList()
+    {
+        return "clothesList";
     }
 
-    @PostMapping("/save")
-    public ClothesDTO saveCloth(@RequestBody ClothesDTO clothToSave) {
-        logger.info("Saving cloth: {}", clothToSave.getName());
-        var cloth = converter.fromDTO(clothToSave);
-        return converter.fromClothes(service.saveCloth(cloth));
-    }
 
-    @DeleteMapping("/remove/{id}")
-    public int removeClothById(@PathVariable Integer id){
-        logger.info("Got id to remove: {}", id);
-        service.removeCloth(id);
-        return id;
-    }
+
 }
---
+++ src/main/java/pl/rdzienio/clothes/ClothesRESTController.java
@@ -0,0 +1,49 @@
+package pl.rdzienio.clothes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clothes")
+public class ClothesRESTController {
+
+    private final ClothesService service;
+    private final ClothesConverter converter;
+    Logger logger = LoggerFactory.getLogger(ClothesRESTController.class);
+
+    public ClothesRESTController(final ClothesService service, ClothesConverter converter) {
+        this.service = service;
+        this.converter = converter;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Clothes>> getAllClothes() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClothesDTO> getById(@PathVariable Integer id) {
+        logger.info("Got id {}", id);
+        Clothes gotById = service.getById(id);
+        logger.info("Found with id {}: {}", id, gotById.getName());
+        return ResponseEntity.ok(converter.fromClothes(gotById));
+    }
+
+    @PostMapping("/save")
+    public ClothesDTO saveCloth(@RequestBody ClothesDTO clothToSave) {
+        logger.info("Saving cloth: {}", clothToSave.getName());
+        var cloth = converter.fromDTO(clothToSave);
+        return converter.fromClothes(service.saveCloth(cloth));
+    }
+
+    @DeleteMapping("/{id}")
+    public int removeClothById(@PathVariable Integer id){
+        logger.info("Got id to remove: {}", id);
+        service.removeCloth(id);
+        return id;
+    }
+}
