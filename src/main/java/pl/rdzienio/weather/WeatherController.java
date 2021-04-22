dc030a05db6c5c0655c19df06ee3208abf4f8cf5
Robert Dzienio <robertdzienio@gmail.com>
2021-04-22 20:20:48 +0200

    [add] weather page

--- src/main/java/pl/rdzienio/weather/WeatherController.java
+++ src/main/java/pl/rdzienio/weather/WeatherController.java
@@ -2,28 +2,29 @@ package pl.rdzienio.weather;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/api/weather")
 public class WeatherController {
 
     private final WeatherService service;
 
-    private final Logger logger = LoggerFactory.getLogger(WeatherController.class);
+    private final Logger logger = LoggerFactory.getLogger(WeatherRESTController.class);
 
-    public WeatherController(final WeatherService service) {
+
+    public WeatherController(WeatherService service) {
         this.service = service;
     }
 
-    @GetMapping(params = {"city"})
-    ResponseEntity<WeatherForecast> showWeatherForCurrentCity(@RequestParam String city) {
-        logger.info("Got request with parameter " + city);
-        return ResponseEntity.ok(service.getWeatherForecast(city));
+    @GetMapping("weather")
+    public String showHomePageForWeather(Model model)
+    {
+        var weatherForWarsaw= service.getWeatherForecast("Warszawa");
+        model.addAttribute("weatherForWarsaw", weatherForWarsaw);
+        model.addAttribute("weatherIconForWarsaw", service.getWeatherIconForCity("Warszawa"));
+        return "weather";
     }
+
 }
---
+++ src/main/java/pl/rdzienio/weather/WeatherRESTController.java
@@ -0,0 +1,28 @@
+package pl.rdzienio.weather;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/api/weather")
+public class WeatherRESTController {
+
+    private final WeatherService service;
+
+    private final Logger logger = LoggerFactory.getLogger(WeatherRESTController.class);
+
+    public WeatherRESTController(final WeatherService service) {
+        this.service = service;
+    }
+
+    @GetMapping(params = {"city"})
+    ResponseEntity<WeatherForecast> showWeatherForCurrentCity(@RequestParam String city) {
+        logger.info("Got request with parameter " + city);
+        return ResponseEntity.ok(service.getWeatherForecast(city));
+    }
+}
